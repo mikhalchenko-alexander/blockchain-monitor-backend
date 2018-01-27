594364542bcc820210cf31e5698f3abb7de47b2f
aam <aam@anahoret.com>
2018-01-27 10:58:33 +0200

    Initial commit.

---
+++ src/main/kotlin/com/anahoret/blockchainmonitorbackend/BlockchainMonitorBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.anahoret.blockchainmonitorbackend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BlockchainMonitorBackendApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BlockchainMonitorBackendApplication::class.java, *args)
+}
